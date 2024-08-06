e53eaf631db380eac440180a0186b22d7a380e69
Kauan Marques <daluzmarkes@gmail.com>
2024-08-06 13:49:34 -0400

    feat: page menu

--- project_pizzaria_newbyte/Pages/MenuWindow.xaml.cs
+++ project_pizzaria_newbyte/Pages/MenuWindow.xaml.cs
@@ -1,4 +1,8 @@
-﻿using System.Text;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -6,17 +10,16 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace project_pizzaria_newbyte
+namespace project_pizzaria_newbyte.Pages
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Lógica interna para MenuWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MenuWindow : Window
     {
-        public MainWindow()
+        public MenuWindow()
         {
             InitializeComponent();
         }
