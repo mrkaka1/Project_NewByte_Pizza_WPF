0e1d64b59f7ef692ef08922c701f2652fdb80cd0
Matheus Andrade da Costa <2021102020080@aluno.ifro.edu.br>
2024-09-24 09:50:05 -0400

    feat: created page search-2

---
+++ project_pizzaria_newbyte/Pages/Search-2.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace project_pizzaria_newbyte
+{
+    /// <summary>
+    /// Interação lógica para Search_2.xam
+    /// </summary>
+    public partial class Search_2 : Page
+    {
+        public Search_2()
+        {
+            InitializeComponent();
+        }
+    }
+}
