cf741d1839620d4a2683000aa0b89070c5e08b18
Kauan Marques da Luz <daluzmarkes@gmail.com>
2024-11-01 18:43:17 -0400

    feat: created page employee registration

--- project_pizzaria_newbyte/DataBase/Connection.cs
+++ project_pizzaria_newbyte/DataBase/Connection.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +10,55 @@ namespace project_pizzaria_newbyte.DataBase
 {
     class Connection
     {
+        internal class Connect
+        {
+            private static string host = "localhost";
+
+            private static string port = "3306";
+
+            //private static string user = "root";
+
+            //private static string password = "root";
+
+            //private static string dbname = "bd_escola";
+
+            private static MySqlConnection connection;
+
+            private static MySqlCommand command;
+
+            public Connect()
+            {
+                try
+                {
+                    connection = new MySqlConnection($"server={host};database={/*dbname*/};port={port};user={/*user*/};password={/*password*/}");
+                    connection.Open();
+
+                }
+                catch (Exception ex)
+                {
+                    throw ex;
+                }
+            }
+
+            public MySqlCommand Query()
+            {
+                try
+                {
+                    command = connection.CreateCommand();
+                    command.CommandType = CommandType.Text;
+
+                    return command;
+                }
+                catch (Exception ex)
+                {
+                    throw ex;
+                }
+            }
+
+            public void Close()
+            {
+                connection.Close();
+            }
+        }
     }
 }
--- project_pizzaria_newbyte/Helpers/DAOHelper.cs
+++ project_pizzaria_newbyte/Helpers/DAOHelper.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +9,39 @@ namespace project_pizzaria_newbyte.Helpers
 {
     class DAOHelper
     {
+        public static string GetString(MySqlDataReader reader, string column_name)
+        {
+            string text = string.Empty;
 
+            if (!reader.IsDBNull(reader.GetOrdinal(column_name)))
+                text = reader.GetString(column_name);
+
+            return text;
+        }
+
+        public static double GetDouble(MySqlDataReader reader, string column_name)
+        {
+            double value = 0.0;
+
+            if (!reader.IsDBNull(reader.GetOrdinal(column_name)))
+                value = reader.GetDouble(column_name);
+
+            return value;
+        }
+
+        public static DateTime? GetDateTime(MySqlDataReader reader, string column_name)
+        {
+            DateTime? value = null;
+
+            if (!reader.IsDBNull(reader.GetOrdinal(column_name)))
+                value = reader.GetDateTime(column_name);
+
+            return value;
+        }
+
+        public static bool IsNull(MySqlDataReader reader, string column_name)
+        {
+            return reader.IsDBNull(reader.GetOrdinal(column_name));
+        }
     }
 }
